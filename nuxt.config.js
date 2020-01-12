e573a64c29f45aa75e9c4223f3d4f4e27b75b47f
gomezmark <>
2020-01-12 14:23:07 +0800

    - debug gh-page

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
     ]
   },
   router: {
-    base: `https://gomezmark.github.io/nuxt-github-page-template/`
+    base: `/nuxt-github-page-template/`
   },
   /*
   ** Customize the progress-bar color
